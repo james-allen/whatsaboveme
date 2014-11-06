5bb39d83b609f8a27a7c228c497bef48b63c2f68
James T. Allen <j.allen@physics.usyd.edu.au>
2014-11-06 20:10:37 +1100

    Made location+object functions into methods; handle bad locations.

--- main.py
+++ main.py
@@ -24,41 +24,6 @@ START_TIME = Time('2000-01-01 12:00:00.0', scale='utc')
 SimbadQuerier = Simbad()
 SimbadQuerier.add_votable_fields('otype')
 
-def get_location(name):
-    req_id = requests.get(
-        GOOGLE_URL_AUTOCOMPLETE,
-        params={'input':name, 'key':GOOGLE_MAPS_API_KEY})
-    place_id = req_id.json()['predictions'][0]['place_id']
-    req_loc = requests.get(
-        GOOGLE_URL_DETAILS,
-        params={'placeid': place_id, 'key':GOOGLE_MAPS_API_KEY})
-    return req_loc.json()['result']['geometry']['location']
-
-def get_ra_dec(location):
-    gst = (18.697374558 + 24.06570982441908 * (Time.now() - START_TIME).value)
-    ra = (gst * 15.0 + location['lng']) % 360                                                      
-    dec = location['lat']                           
-    return {'ra': ra, 'dec': dec}
-
-# def get_object(coords_dict):
-#     coords = coordinates.SkyCoord(
-#         ra=coords_dict['ra'], dec=coords_dict['dec'], unit=(u.deg, u.deg))
-#     ned_result = Ned.query_region(coords, radius=0.1*u.deg)
-#     closest_object = ned_result[np.argmin(ned_result['Distance (arcmin)'])]
-#     object_name = closest_object['Object Name']
-#     return object_name
-
-def get_object(coords_dict):
-    coords = coordinates.SkyCoord(
-        ra=coords_dict['ra'], dec=coords_dict['dec'], unit=(u.deg, u.deg))
-    simbad_result = SimbadQuerier.query_region(coords, radius=0.1*u.deg)
-    coords_result = coordinates.SkyCoord(
-        ra=simbad_result['RA'], dec=simbad_result['DEC'], unit=(u.hour, u.deg))
-    closest_object = simbad_result[np.argmin(coords_result)]
-    object_name = closest_object['MAIN_ID']
-    object_type = closest_object['OTYPE']
-    return {'name': object_name, 'type': object_type}
-
 # The following is an attempt to get an image directly out of Aladin. Needs work.
 # http://cdsportal.u-strasbg.fr/AladinPoolServlet/AladinPoolServlet?script=setconf%20cm%3Dnoreverse%3Breticle%20off%3Bscale%20off%3Bget%20aladin%28POSSII/F/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bget%20aladin%28POSSII/J/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bsync%3Bzoom%202x%3Brgb%201%202%3Bsync%3Bgrid%20off%3Bsave%20-png%20768x768%3Bquit
 
@@ -85,14 +50,56 @@ class Bot(object):
     def process_tweet(self, tweet):
         """Process and reply to a tweet."""
         text = tweet['text'].lower().replace('@whatsaboveme', '')
-        location = get_location(text)
-        ra_dec = get_ra_dec(location)
-        obj = get_object(ra_dec)
-        reply_text = '@{} {}, a {}, is above you.'.format(
-            tweet['user']['screen_name'], obj['name'], obj['type'])
+        try:
+            location = self.get_location(text)
+        except LocationNotFoundError:
+            message = "I'm sorry, I can't find your location."
+        else:
+            ra_dec = self.get_ra_dec(location)
+            obj = self.get_object(ra_dec)
+            message = '{}, a {}, is above you.'.format(
+                obj['name'], obj['type'])
+        reply_text = '@{} {}'.format(tweet['user']['screen_name'], message)
         update = self.api.request(
             'statuses/update', 
             {'status': reply_text,
              'in_reply_to_status_id': tweet['id']})
+
+    def get_location(self, name):
+        req_id = requests.get(
+            GOOGLE_URL_AUTOCOMPLETE,
+            params={'input':name, 'key':GOOGLE_MAPS_API_KEY})
+        result = req_id.json()
+        if result['status'] == 'ZERO_RESULTS':
+            raise LocationNotFoundError(name)
+        place_id = result['predictions'][0]['place_id']
+        req_loc = requests.get(
+            GOOGLE_URL_DETAILS,
+            params={'placeid': place_id, 'key':GOOGLE_MAPS_API_KEY})
+        return req_loc.json()['result']['geometry']['location']
+
+    def get_ra_dec(self, location):
+        gst = (18.697374558 + 24.06570982441908 * (Time.now() - START_TIME).value)
+        ra = (gst * 15.0 + location['lng']) % 360                                                      
+        dec = location['lat']                           
+        return {'ra': ra, 'dec': dec}
+
+    def get_object(self, coords_dict):
+        coords = coordinates.SkyCoord(
+            ra=coords_dict['ra'], dec=coords_dict['dec'], unit=(u.deg, u.deg))
+        simbad_result = SimbadQuerier.query_region(coords, radius=1.0*u.deg)
+        coords_result = coordinates.SkyCoord(
+            ra=simbad_result['RA'], dec=simbad_result['DEC'], unit=(u.hour, u.deg))
+        closest_object = simbad_result[np.argmin(coords_result)]
+        object_name = closest_object['MAIN_ID']
+        object_type = closest_object['OTYPE']
+        return {'name': object_name, 'type': object_type}
+
+
+class BotError(Exception):
+    pass
+
+class LocationNotFoundError(BotError):
+    pass
 
 
