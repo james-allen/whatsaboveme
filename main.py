4a7154861031f3bc2f18900484d1832fbff5cebb
James T. Allen <j.allen@physics.usyd.edu.au>
2014-11-06 19:54:29 +1100

    Wrote twitterbot.

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ GOOGLE_URL_DETAILS = 'https://maps.googleapis.com/maps/api/place/details/json'
 
 GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
+TWITTER_CONSUMER_KEY = None
+TWITTER_CONSUMER_SECRET = None
+TWITTER_ACCESS_TOKEN_KEY = None
+TWITTER_ACCESS_TOKEN_SECRET = None
+
 START_TIME = Time('2000-01-01 12:00:00.0', scale='utc')
 
 SimbadQuerier = Simbad()
@@ -56,4 +61,38 @@ def get_object(coords_dict):
 
 # The following is an attempt to get an image directly out of Aladin. Needs work.
 # http://cdsportal.u-strasbg.fr/AladinPoolServlet/AladinPoolServlet?script=setconf%20cm%3Dnoreverse%3Breticle%20off%3Bscale%20off%3Bget%20aladin%28POSSII/F/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bget%20aladin%28POSSII/J/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bsync%3Bzoom%202x%3Brgb%201%202%3Bsync%3Bgrid%20off%3Bsave%20-png%20768x768%3Bquit
+
+class Bot(object):
+    """The WhatsAboveMe twitterbot."""
+
+    def __init__(self, 
+                 consumer_key=TWITTER_CONSUMER_KEY,
+                 consumer_secret=TWITTER_CONSUMER_SECRET,
+                 access_token_key=TWITTER_ACCESS_TOKEN_KEY,
+                 access_token_secret=TWITTER_ACCESS_TOKEN_SECRET):
+        self.api = TwitterAPI(
+            consumer_key, consumer_secret,
+            access_token_key, access_token_secret)
+        self.stream = None
+
+    def activate(self):
+        """Switch the bot on."""
+        self.stream = self.api.request(
+            'statuses/filter', {'track': '@whatsaboveme'})
+        for tweet in self.stream:
+            self.process_tweet(tweet)
+
+    def process_tweet(self, tweet):
+        """Process and reply to a tweet."""
+        text = tweet['text'].lower().replace('@whatsaboveme', '')
+        location = get_location(text)
+        ra_dec = get_ra_dec(location)
+        obj = get_object(ra_dec)
+        reply_text = '@{} {}, a {}, is above you.'.format(
+            tweet['user']['screen_name'], obj['name'], obj['type'])
+        update = self.api.request(
+            'statuses/update', 
+            {'status': reply_text,
+             'in_reply_to_status_id': tweet['id']})
+
 
