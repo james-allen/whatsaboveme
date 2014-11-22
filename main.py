857135482b6bbcc388bb6e8bac0d835f8add3f7c
James T. Allen <j.allen@physics.usyd.edu.au>
2014-11-22 15:18:58 +1000

    Return magnitude of object.

--- main.py
+++ main.py
@@ -57,9 +57,6 @@ START_TIME = Time('2000-01-01 12:00:00.0', scale='utc')
 
 TZ_DICT = {"International Date Line West": "Pacific/Midway", "Midway Island": "Pacific/Midway", "American Samoa": "Pacific/Pago_Pago", "Hawaii": "Pacific/Honolulu", "Alaska": "America/Juneau", "Pacific Time (US & Canada)": "America/Los_Angeles", "Tijuana": "America/Tijuana", "Mountain Time (US & Canada)": "America/Denver", "Arizona": "America/Phoenix", "Chihuahua": "America/Chihuahua", "Mazatlan": "America/Mazatlan", "Central Time (US & Canada)": "America/Chicago", "Saskatchewan": "America/Regina", "Guadalajara": "America/Mexico_City", "Mexico City": "America/Mexico_City", "Monterrey": "America/Monterrey", "Central America": "America/Guatemala", "Eastern Time (US & Canada)": "America/New_York", "Indiana (East)": "America/Indiana/Indianapolis", "Bogota": "America/Bogota", "Lima": "America/Lima", "Quito": "America/Lima", "Atlantic Time (Canada)": "America/Halifax", "Caracas": "America/Caracas", "La Paz": "America/La_Paz", "Santiago": "America/Santiago", "Newfoundland": "America/St_Johns", "Brasilia": "America/Sao_Paulo", "Buenos Aires": "America/Argentina/Buenos_Aires", "Montevideo": "America/Montevideo", "Georgetown": "America/Guyana", "Greenland": "America/Godthab", "Mid-Atlantic": "Atlantic/South_Georgia", "Azores": "Atlantic/Azores", "Cape Verde Is.": "Atlantic/Cape_Verde", "Dublin": "Europe/Dublin", "Edinburgh": "Europe/London", "Lisbon": "Europe/Lisbon", "London": "Europe/London", "Casablanca": "Africa/Casablanca", "Monrovia": "Africa/Monrovia", "UTC": "Etc/UTC", "Belgrade": "Europe/Belgrade", "Bratislava": "Europe/Bratislava", "Budapest": "Europe/Budapest", "Ljubljana": "Europe/Ljubljana", "Prague": "Europe/Prague", "Sarajevo": "Europe/Sarajevo", "Skopje": "Europe/Skopje", "Warsaw": "Europe/Warsaw", "Zagreb": "Europe/Zagreb", "Brussels": "Europe/Brussels", "Copenhagen": "Europe/Copenhagen", "Madrid": "Europe/Madrid", "Paris": "Europe/Paris", "Amsterdam": "Europe/Amsterdam", "Berlin": "Europe/Berlin", "Bern": "Europe/Berlin", "Rome": "Europe/Rome", "Stockholm": "Europe/Stockholm", "Vienna": "Europe/Vienna", "West Central Africa": "Africa/Algiers", "Bucharest": "Europe/Bucharest", "Cairo": "Africa/Cairo", "Helsinki": "Europe/Helsinki", "Kyiv": "Europe/Kiev", "Riga": "Europe/Riga", "Sofia": "Europe/Sofia", "Tallinn": "Europe/Tallinn", "Vilnius": "Europe/Vilnius", "Athens": "Europe/Athens", "Istanbul": "Europe/Istanbul", "Minsk": "Europe/Minsk", "Jerusalem": "Asia/Jerusalem", "Harare": "Africa/Harare", "Pretoria": "Africa/Johannesburg", "Moscow": "Europe/Moscow", "St. Petersburg": "Europe/Moscow", "Volgograd": "Europe/Moscow", "Kuwait": "Asia/Kuwait", "Riyadh": "Asia/Riyadh", "Nairobi": "Africa/Nairobi", "Baghdad": "Asia/Baghdad", "Tehran": "Asia/Tehran", "Abu Dhabi": "Asia/Muscat", "Muscat": "Asia/Muscat", "Baku": "Asia/Baku", "Tbilisi": "Asia/Tbilisi", "Yerevan": "Asia/Yerevan", "Kabul": "Asia/Kabul", "Ekaterinburg": "Asia/Yekaterinburg", "Islamabad": "Asia/Karachi", "Karachi": "Asia/Karachi", "Tashkent": "Asia/Tashkent", "Chennai": "Asia/Kolkata", "Kolkata": "Asia/Kolkata", "Mumbai": "Asia/Kolkata", "New Delhi": "Asia/Kolkata", "Kathmandu": "Asia/Kathmandu", "Astana": "Asia/Dhaka", "Dhaka": "Asia/Dhaka", "Sri Jayawardenepura": "Asia/Colombo", "Almaty": "Asia/Almaty", "Novosibirsk": "Asia/Novosibirsk", "Rangoon": "Asia/Rangoon", "Bangkok": "Asia/Bangkok", "Hanoi": "Asia/Bangkok", "Jakarta": "Asia/Jakarta", "Krasnoyarsk": "Asia/Krasnoyarsk", "Beijing": "Asia/Shanghai", "Chongqing": "Asia/Chongqing", "Hong Kong": "Asia/Hong_Kong", "Urumqi": "Asia/Urumqi", "Kuala Lumpur": "Asia/Kuala_Lumpur", "Singapore": "Asia/Singapore", "Taipei": "Asia/Taipei", "Perth": "Australia/Perth", "Irkutsk": "Asia/Irkutsk", "Ulaanbaatar": "Asia/Ulaanbaatar", "Seoul": "Asia/Seoul", "Osaka": "Asia/Tokyo", "Sapporo": "Asia/Tokyo", "Tokyo": "Asia/Tokyo", "Yakutsk": "Asia/Yakutsk", "Darwin": "Australia/Darwin", "Adelaide": "Australia/Adelaide", "Canberra": "Australia/Melbourne", "Melbourne": "Australia/Melbourne", "Sydney": "Australia/Sydney", "Brisbane": "Australia/Brisbane", "Hobart": "Australia/Hobart", "Vladivostok": "Asia/Vladivostok", "Guam": "Pacific/Guam", "Port Moresby": "Pacific/Port_Moresby", "Magadan": "Asia/Magadan", "Solomon Is.": "Pacific/Guadalcanal", "New Caledonia": "Pacific/Noumea", "Fiji": "Pacific/Fiji", "Kamchatka": "Asia/Kamchatka", "Marshall Is.": "Pacific/Majuro", "Auckland": "Pacific/Auckland", "Wellington": "Pacific/Auckland", "Nuku'alofa": "Pacific/Tongatapu", "Tokelau Is.": "Pacific/Fakaofo", "Chatham Is.": "Pacific/Chatham", "Samoa": "Pacific/Apia"}
 
-SimbadQuerier = Simbad()
-SimbadQuerier.add_votable_fields('otype')
-
 # The following is an attempt to get an image directly out of Aladin. Needs work.
 # http://cdsportal.u-strasbg.fr/AladinPoolServlet/AladinPoolServlet?script=setconf%20cm%3Dnoreverse%3Breticle%20off%3Bscale%20off%3Bget%20aladin%28POSSII/F/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bget%20aladin%28POSSII/J/DSS2%29%2013%2029%2042.4%20%2B47%2011%2041%3Bsync%3Bzoom%202x%3Brgb%201%202%3Bsync%3Bgrid%20off%3Bsave%20-png%20768x768%3Bquit
 
@@ -79,6 +76,12 @@ class Bot(object):
         except IOError:
             self.arrow = Image.open('arrow.png')
         self.arrow_offset = arrow_offset
+        # The following list is in descending order of preference
+        self.filternames = ['V', 'r', 'B', 'g', 'R', 'i', 'U', 'u', 'I', 'z']
+        self.simbad = Simbad()
+        self.simbad.add_votable_fields('otype', 'ze')
+        self.simbad.add_votable_fields(
+            *['flux({})'.format(f) for f in self.filternames])
 
     def activate(self):
         """Switch the bot on."""
@@ -161,7 +164,7 @@ class Bot(object):
     def get_object(self, coords_dict):
         coords = coordinates.SkyCoord(
             ra=coords_dict['ra'], dec=coords_dict['dec'], unit=(u.deg, u.deg))
-        simbad_result = SimbadQuerier.query_region(coords, radius=0.25*u.deg)
+        simbad_result = self.simbad.query_region(coords, radius=0.25*u.deg)
         print 'Simbad results received: {} objects'.format(len(simbad_result))
         keep = np.array([bool(re.match(r'.+ .+ .+\..+', line['RA'])) and
                          bool(re.match(r'.+ .+ .+\..+', line['DEC']))
@@ -172,11 +175,20 @@ class Bot(object):
             unit=(u.hour, u.deg))
         idx = np.argmin(coords_result)
         closest_object = trimmed_result[idx]
-        object_name = closest_object['MAIN_ID']
-        object_type = closest_object['OTYPE']
-        print 'Object found: {}, {}'.format(object_name, object_type)
-        return {'name': object_name, 'type': object_type,
-                'coords': coords_result[idx]}
+        obj = {
+            'name': closest_object['MAIN_ID'],
+            'type': closest_object['OTYPE'],
+            'coords': coords_result[idx],
+        }
+        for filt in self.filternames:
+            key = 'FLUX_' + filt
+            if closest_object[key]:
+                obj['mag'] = closest_object[key]
+                break
+        else:
+            obj['mag'] = None
+        print 'Object found: {}, {}'.format(obj['name'], obj['type'])
+        return obj
 
     def get_sky_image(self, coords):
         """Return a jpeg PIL Image downloaded from Aladin."""
